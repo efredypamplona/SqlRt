1aa8993f4b137042b41b8c7de6547de97a3e00a4
efredy pamplona <efredypamplona@gmail.com>
2025-03-28 17:03:00 -0500

    actualizacion de consulta para ver el esquema de la tabla

--- Consultas.sql
+++ Consultas.sql
@@ -1,1 +1,8 @@
+--muestra la tabla que quiero ver
 select * from [Sales].[SalesOrderHeader]
+-- muestra el nombre de las tablas 
+SELECT name from sys.tables
+--verifica el esquema de la tabla
+SELECT TABLE_SCHEMA, TABLE_NAME 
+FROM INFORMATION_SCHEMA.TABLES 
+WHERE TABLE_NAME = 'SalesOrderHeader';
