d75abcc557514d49876b68418dddd372357a26a3
efredy pamplona <efredypamplona@gmail.com>
2025-03-29 18:59:08 -0500

    se realizan nuevas consultas para estudiar

--- Consultas.sql
+++ Consultas.sql
@@ -19,3 +19,85 @@ SELECT
 FROM Sales.SalesOrderHeader AS soh
 INNER JOIN Sales.SalesOrderDetail AS sod
     ON soh.SalesOrderID = sod.SalesOrderID;
+
+
+SELECT * from sales.SalesOrderHeader
+SELECT * from sales.SalesOrderDetail
+
+SELECT 
+    soh.SalesOrderID, 
+    soh.OrderDate, 
+    sod.ProductID, 
+    sod.OrderQty, 
+    sod.LineTotal
+FROM Sales.SalesOrderHeader AS soh
+LEFT JOIN Sales.SalesOrderDetail AS sod
+    ON soh.SalesOrderID = sod.SalesOrderID;
+
+
+SELECT 
+    soh.SalesOrderID, 
+    soh.OrderDate, 
+    sod.ProductID, 
+    sod.OrderQty, 
+    sod.LineTotal
+FROM Sales.SalesOrderHeader AS soh
+FULL JOIN Sales.SalesOrderDetail AS sod
+    ON soh.SalesOrderID = sod.SalesOrderID;
+
+
+SELECT CustomerID, COUNT(SalesOrderID) AS TotalPedidos
+FROM Sales.SalesOrderHeader
+GROUP BY CustomerID
+HAVING COUNT(SalesOrderID) > 5;
+
+
+-- Escribe una consulta para obtener los primeros 10 registros de una tabla llamada Sales.SalesOrderHeader.
+SELECT * FROM Sales.SalesOrderHeader
+WHERE SalesOrderID
+BETWEEN 43659 AND 43668
+
+
+-- ¿Cómo filtras los productos cuyo nombre comienza con "A" en la tabla Production.Product?
+SELECT * FROM  Production.Product
+
+
+SELECT TOP 10 * FROM Sales.SalesOrderHeader
+
+SELECT * 
+FROM Sales.SalesOrderHeader
+ORDER BY OrderDate DESC
+OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY;
+
+
+
+SELECT * 
+FROM (
+    SELECT TOP 10 * 
+    FROM Sales.SalesOrderHeader
+    ORDER BY OrderDate ASC
+) AS SubQuery
+ORDER BY OrderDate DESC;
+
+SELECT TOP 10 * 
+    FROM Sales.SalesOrderHeader
+    ORDER BY OrderDate ASC
+
+--¿Cómo filtras los productos cuyo nombre comienza con "A" en la tabla Production.Product?
+SELECT * FROM Production.Product
+WHERE NAME  LIKE 'A%'
+
+
+
+SET STATISTICS TIME ON;
+
+SELECT * 
+FROM Sales.SalesOrderDetail 
+WHERE ProductID = 776;
+
+SET STATISTICS TIME OFF;
+
+CREATE NONCLUSTERED INDEX idx_ProductID 
+ON Sales.SalesOrderDetail (ProductID);
+
+
